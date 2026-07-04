85ab5fb3d56fd50ca77a91075ea877e39a3aa4d0
Skebzy <skebzzy@gmail.com>
2026-07-04 20:37:45 +0530

    Output cleanup

--- src/ui/colors.rs
+++ src/ui/colors.rs
@@ -4,21 +4,21 @@ use owo_colors::OwoColorize;
 
 /// Print a step/message before an action.
 pub fn step(msg: &str) {
-    println!("{} {}", "⏳".blue(), msg);
+    println!("{}", msg);
 }
 
 /// Print an informational message.
 #[allow(dead_code)]
 pub fn info(msg: &str) {
-    println!("{} {}", "ℹ".blue(), msg);
+    println!("{}", msg);
 }
 
 /// Print a success message.
 pub fn success(msg: &str) {
-    println!("{} {}", "✔".green(), msg);
+    println!("{}", msg);
 }
 
 /// Print an error message to stderr.
 pub fn error(msg: &str) {
-    eprintln!("{} {}", "✖".red(), msg);
+    eprintln!("{}", msg);
 }
